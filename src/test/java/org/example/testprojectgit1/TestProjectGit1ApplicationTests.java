cf5736f881403af06ca0b3fa15ced81ed7a1ebb2
Szymon <szymon@mbp-szymon.home>
2024-01-26 18:27:19 +0100

    First, initial commit.

---
+++ src/test/java/org/example/testprojectgit1/TestProjectGit1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.testprojectgit1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProjectGit1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
